c3036c9d3efdc994cc5208593bc0cb47f6348253
sky <shn7446@duksung.ac.kr>
2021-12-16 08:15:01 +0900

    Post 모델 생성하고 시간설정하기

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    #head_image
+    price = models.IntegerField()
+    publisher = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    #category
+    release_date = models.DateField()
+    can_multi_play = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
