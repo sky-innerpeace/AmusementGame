20d2112a8dd6b9b3d0fe15a7761282c69abcf472
sky <shn7446@duksung.ac.kr>
2021-12-16 10:09:34 +0900

    FBV로 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Game
+
+
+def index(request):
+    games = Game.objects.all().order_by('-pk')
+    return render(request, 'blog/index.html',
+                  {
+                      'games' : games
+                  }
+                  )
+
+def single_game_page(request, pk):
+    game = Game.objects.get(pk=pk)
+    return render(request, 'blog/single_game_page.html',
+                  {
+                      'game' : game
+                  }
+                  )
