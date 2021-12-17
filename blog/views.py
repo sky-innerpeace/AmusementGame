a3c235e50f74a53d39dd12be6fa68473e823b40a
sky <shn7446@duksung.ac.kr>
2021-12-18 02:04:06 +0900

    게임 목록 페이지에 pagination 적용

--- blog/views.py
+++ blog/views.py
@@ -30,6 +30,7 @@ def new_comment(request, pk):
 class GameList(ListView):
     model = Game
     ordering = '-pk'
+    paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context=super(GameList, self).get_context_data()
